b1e9232964712406a9ae603e9b2759fdc2237108
dieterjava <md_hermann284%%>
2021-02-16 19:02:44 +0100

    Add Python

--- Python.mm
+++ Python.mm
@@ -8,6 +8,9 @@
 <node CREATED="1613466753911" ID="ID_1197893401" MODIFIED="1613466755691" TEXT="sorting"/>
 <node CREATED="1613466560280" ID="ID_445319755" MODIFIED="1613466562316" TEXT="slicing"/>
 <node CREATED="1613466562992" ID="ID_1571389714" MODIFIED="1613466568283" TEXT="filtering"/>
+<node CREATED="1613475536534" ID="ID_1007340016" MODIFIED="1613476686756" TEXT="merge">
+<icon BUILTIN="help"/>
+</node>
 </node>
 </node>
 <node CREATED="1613466229960" ID="ID_1254173818" MODIFIED="1613466232851" TEXT="matplotlib"/>
@@ -34,32 +37,51 @@
 <node CREATED="1613466374871" ID="ID_1835053416" MODIFIED="1613466421180" TEXT="Interactive coding with the interpreter at any point in the program"/>
 </node>
 <node CREATED="1613465918831" HGAP="105" ID="ID_1695297930" MODIFIED="1613470664157" POSITION="left" TEXT="Wikipedia" VSHIFT="-85">
+<node CREATED="1613476192078" ID="ID_1172439569" LINK="https://de.wikipedia.org/wiki/Python_(Programmiersprache)" MODIFIED="1613476304066" TEXT="https://de.wikipedia.org/wiki/Python_(Programmiersprache)"/>
+<node CREATED="1613476287934" ID="ID_546783662" LINK="https://en.wikipedia.org/wiki/Python_(programming_language)" MODIFIED="1613476295314" TEXT="https://en.wikipedia.org/wiki/Python_(programming_language)"/>
 <node CREATED="1613465963532" ID="ID_372023985" MODIFIED="1613465996947" TEXT="Interpreter"/>
 <node CREATED="1613466006039" ID="ID_1562189358" MODIFIED="1613466024699" TEXT="Coding, Indendation important"/>
 <node CREATED="1613466124415" ID="ID_746983624" MODIFIED="1613466142091" TEXT="C, Dlls behing, no virtual machine like in Java"/>
+<node CREATED="1613476034918" ID="ID_1279476358" MODIFIED="1613476035907" TEXT="Strukturierung durch Einr&#xfc;cken"/>
+<node CREATED="1613475987118" ID="ID_699073379" MODIFIED="1613475988013" TEXT="Sammeltypen, darunter Listen, Tupel, Mengen (Sets) und assoziative Arrays (Dictionaries)"/>
+<node CREATED="1613475869542" ID="ID_486659993" MODIFIED="1613475898299" TEXT="Datentypen werden dynamisch vergeben, so wie bei Smalltalk oder Lisp &#x2013; und nicht wie bei Java"/>
+<node CREATED="1613475971494" ID="ID_1894065044" MODIFIED="1613475972531" TEXT="Python unterst&#xfc;tzt Mehrfachvererbung"/>
+<node CREATED="1613476437126" ID="ID_593493957" LINK="https://en.wikipedia.org/wiki/Zen_of_Python" MODIFIED="1613476592819" TEXT="https://en.wikipedia.org/wiki/Zen_of_Python">
+<icon BUILTIN="button_ok"/>
 </node>
-<node CREATED="1613466094144" HGAP="73" ID="ID_1129651020" MODIFIED="1613470670574" POSITION="left" TEXT="ML Machine Learning" VSHIFT="-24">
-<node CREATED="1613466645399" ID="ID_1519597188" MODIFIED="1613466651132" TEXT="TensorFlow from Google">
-<node CREATED="1613470610499" ID="ID_6156278" LINK="https://playground.tensorflow.org/" MODIFIED="1613470619135" TEXT="https://playground.tensorflow.org/"/>
+</node>
+<node CREATED="1613466094144" HGAP="73" ID="ID_1129651020" MODIFIED="1613476729352" POSITION="left" TEXT="ML Machine Learning" VSHIFT="-24">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1613466645399" ID="ID_1519597188" MODIFIED="1613476602978" TEXT="TensorFlow from Google">
+<icon BUILTIN="help"/>
+<node CREATED="1613470610499" ID="ID_6156278" LINK="https://playground.tensorflow.org/" MODIFIED="1613476648425" TEXT="https://playground.tensorflow.org/">
+<icon BUILTIN="button_ok"/>
+<icon BUILTIN="idea"/>
+</node>
 </node>
 <node CREATED="1613468614593" ID="ID_853144233" MODIFIED="1613469211606" TEXT="scikit-learn">
 <node CREATED="1613469212986" ID="ID_844406260" MODIFIED="1613469217895" TEXT="Decision Trees">
 <node CREATED="1613469219226" ID="ID_1052219344" LINK="https://scikit-learn.org/stable/auto_examples/index.html#decision-trees" MODIFIED="1613469231849" TEXT="example at https://scikit-learn.org/stable/auto_examples/index.html#decision-trees"/>
 <node CREATED="1613469295841" ID="ID_1149791782" MODIFIED="1613469312678" TEXT="Decision Trees, Online Tool and Editor, free"/>
-<node CREATED="1613469313881" ID="ID_112817714" LINK="https://github.com/SilverDecisions/SilverDecisions/wiki/Gallery" MODIFIED="1613469319888" TEXT="https://github.com/SilverDecisions/SilverDecisions/wiki/Gallery"/>
+<node CREATED="1613469313881" ID="ID_112817714" LINK="https://github.com/SilverDecisions/SilverDecisions/wiki/Gallery" MODIFIED="1613476637937" TEXT="https://github.com/SilverDecisions/SilverDecisions/wiki/Gallery">
+<icon BUILTIN="idea"/>
+</node>
 </node>
 </node>
 <node CREATED="1613468774264" ID="ID_249194087" MODIFIED="1613468775998" TEXT="pytorch"/>
 <node CREATED="1613468776459" ID="ID_134170427" MODIFIED="1613468780910" TEXT="keras"/>
 <node CREATED="1613468365458" ID="ID_187794469" LINK="https://www.zeolearn.com/magazine/python-libraries-for-machine-learning" MODIFIED="1613468392688" TEXT="https://www.zeolearn.com/magazine/python-libraries-for-machine-learning"/>
-<node CREATED="1613469964570" ID="ID_829213977" MODIFIED="1613469968030" TEXT="Use Cases">
+<node CREATED="1613469964570" ID="ID_829213977" MODIFIED="1613476678210" TEXT="Use Cases">
+<icon BUILTIN="idea"/>
 <node CREATED="1613470023234" ID="ID_1914574463" MODIFIED="1613470036879" TEXT="Decision Trees ">
 <node CREATED="1613470038298" ID="ID_191968107" LINK="https://live.yworks.com/demos/complete/decisiontree/index.html" MODIFIED="1613470046568" TEXT="https://live.yworks.com/demos/complete/decisiontree/index.html"/>
 </node>
 </node>
 </node>
 <node CREATED="1613466465135" ID="ID_1780740731" MODIFIED="1613466475780" POSITION="left" TEXT="Web Programming Django Framework">
-<node CREATED="1613468477496" ID="ID_677829985" MODIFIED="1613468489653" TEXT="IDE for web dev with Django ???"/>
+<node CREATED="1613468477496" ID="ID_677829985" MODIFIED="1613474110730" TEXT="PyCharm Professional IDE for Django Web Development">
+<node CREATED="1613474443141" ID="ID_1235166401" LINK="https://www.youtube.com/watch?v=RFM6w8MeICI" MODIFIED="1613474449172" TEXT="https://www.youtube.com/watch?v=RFM6w8MeICI"/>
+</node>
 <node CREATED="1613471994124" ID="ID_1179454850" MODIFIED="1613472006240" TEXT="Django Templates, like jsps">
 <node CREATED="1613472007483" ID="ID_399548743" LINK="https://docs.djangoproject.com/en/3.1/intro/overview/#design-your-templates" MODIFIED="1613472013855" TEXT="https://docs.djangoproject.com/en/3.1/intro/overview/#design-your-templates"/>
 </node>
@@ -79,7 +101,9 @@
 <node CREATED="1613467124983" ID="ID_61658394" MODIFIED="1613467128684" TEXT="Integrated">
 <node CREATED="1613468555434" ID="ID_592016188" LINK="https://docs.python.org/3/library/sqlite3.html" MODIFIED="1613468562337" TEXT="https://docs.python.org/3/library/sqlite3.html"/>
 </node>
-<node CREATED="1613467129080" ID="ID_784231047" MODIFIED="1613467132484" TEXT="In memory use"/>
+<node CREATED="1613467129080" ID="ID_784231047" MODIFIED="1613476655665" TEXT="In memory use">
+<icon BUILTIN="idea"/>
+</node>
 </node>
 </node>
 </node>
