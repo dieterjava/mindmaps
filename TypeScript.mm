b79e3b8aa32d90d69a295bd3b724d18b74418367
dieterjava <md_hermann284%%>
2021-09-27 19:48:53 +0200

    added TypeScript

---
+++ TypeScript.mm
@@ -0,0 +1,39 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1632753219276" ID="ID_21196563" MODIFIED="1632753859892" STYLE="fork" TEXT="TypeScript">
+<node CREATED="1632753227271" HGAP="65" ID="ID_1298577076" MODIFIED="1632753916012" POSITION="right" TEXT="from Microsoft" VSHIFT="-10"/>
+<node CREATED="1632753877134" HGAP="80" ID="ID_1829185326" MODIFIED="1632753919517" POSITION="right" TEXT="VSCode, Visual Studio Code Editor" VSHIFT="-182">
+<node CREATED="1632753893070" ID="ID_868732232" MODIFIED="1632753898775" TEXT="Editor from Microsoft"/>
+</node>
+<node CREATED="1632753232255" ID="ID_786033847" MODIFIED="1632753249961" POSITION="right" TEXT="*.ts compiled to *.js"/>
+<node CREATED="1632753342780" ID="ID_421590242" MODIFIED="1632753349052" POSITION="right" TEXT="Book from OReilly ">
+<node CREATED="1632753350145" ID="ID_831064787" LINK="https://learning.oreilly.com/library/view/mastering-typescript/9781800564732/Text/Chapter_02.xhtml " MODIFIED="1632753354072" TEXT=""/>
+</node>
+<node CREATED="1632753376476" ID="ID_1993057345" MODIFIED="1632753377781" POSITION="right" TEXT="let"/>
+<node CREATED="1632753378487" ID="ID_351644024" MODIFIED="1632753379445" POSITION="right" TEXT="any"/>
+<node CREATED="1632753380175" ID="ID_34580951" MODIFIED="1632753381370" POSITION="right" TEXT="union"/>
+<node CREATED="1632753382054" ID="ID_1515183836" MODIFIED="1632753383313" POSITION="right" TEXT="const"/>
+<node CREATED="1632753407648" ID="ID_155747351" MODIFIED="1632753413762" POSITION="right" TEXT="Optional Chaining">
+<node CREATED="1632753414454" ID="ID_609401513" MODIFIED="1632753419358" TEXT="example: obj?.nestedProperty?.name"/>
+</node>
+<node CREATED="1632753736831" HGAP="64" ID="ID_888633949" MODIFIED="1632753863191" POSITION="left" TEXT="node, npm" VSHIFT="-163">
+<node CREATED="1632753761279" ID="ID_994605156" MODIFIED="1632753766183" TEXT="Semantic versioning">
+<node CREATED="1632753767238" ID="ID_64985883" MODIFIED="1632753768749" TEXT="https://semver.npmjs.com/">
+<node CREATED="1632753771462" ID="ID_793522122" LINK="https://semver.npmjs.com/" MODIFIED="1632753776774" TEXT=""/>
+</node>
+<node CREATED="1632753838581" ID="ID_1972304662" MODIFIED="1632753840770" TEXT="https://docs.npmjs.com/about-semantic-versioning">
+<node CREATED="1632753842743" ID="ID_564981873" LINK="https://docs.npmjs.com/about-semantic-versioning" MODIFIED="1632753845540" TEXT=""/>
+</node>
+</node>
+</node>
+<node CREATED="1632753472375" ID="ID_1113082754" MODIFIED="1632753493550" POSITION="left" TEXT="Classes, Interface, Modules">
+<node CREATED="1632753514484" ID="ID_69203459" MODIFIED="1632753516087" TEXT="Classes and simple inheritance super Abstract classes instanceof modules ( libraries )."/>
+</node>
+<node CREATED="1632753570549" ID="ID_1660273381" MODIFIED="1632753576447" POSITION="left" TEXT="@ Annotations">
+<node CREATED="1632753577134" ID="ID_1092169764" MODIFIED="1632753578047" TEXT="Decorators in TypeScript === Annotations in java "/>
+<node CREATED="1632753583678" ID="ID_778919888" MODIFIED="1632753592549" TEXT="Class decorators: These are decorators that can be applied to a class definition Property decorators: These are decorators that can be applied to a property within a class Method decorators: These are decorators that can be applied to a method on a class Parameter decorators: These are decorators that can be applied to a parameter of a method within a class"/>
+</node>
+<node CREATED="1632753946536" ID="ID_102165859" MODIFIED="1632753950790" POSITION="left" TEXT="Generics"/>
+<node CREATED="1632753951574" ID="ID_616414210" MODIFIED="1632753956006" POSITION="left" TEXT="Injection"/>
+</node>
+</map>
