c627b576550d043d336de6655e49fc8c1f7b8e8d
dieterjava <md_hermann284%%>
2021-09-27 08:44:43 +0200

    added Javascript

--- Cloud_and_docker.mm
+++ Cloud_and_docker.mm
@@ -1,6 +1,6 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1614323210289" ID="ID_1985256380" MODIFIED="1614323220609" TEXT="Cloud and docker">
+<node CREATED="1614323210289" ID="ID_1985256380" MODIFIED="1632302514988" TEXT="Cloud and docker">
 <node CREATED="1614323243271" ID="ID_466693808" MODIFIED="1614323247674" POSITION="right" TEXT="docker on linux"/>
 <node CREATED="1614323248375" ID="ID_1049870184" MODIFIED="1614323263362" POSITION="right" TEXT="GCP Google Cloud Plattform">
 <node CREATED="1614323266855" ID="ID_1559467070" MODIFIED="1614323286954" TEXT="Every with Android Mobile can have one"/>
