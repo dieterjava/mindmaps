092946b6f3546681ab595d6a1829f48d0acbbebb
dieterjava <md_hermann284%%>
2021-09-28 09:08:26 +0200

    more Javascript, libraries, examples, docu

---
+++ Angular.mm
@@ -0,0 +1,26 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1632811639895" ID="ID_422701187" MODIFIED="1632811647385" TEXT="Angular">
+<node CREATED="1632304661182" ID="ID_772422932" LINK="https://www.youtube.com/watch?v=H-sMmxfNxBM" MODIFIED="1632304771593" POSITION="right" TEXT="Debugging">
+<node CREATED="1632304720294" ID="ID_896538836" LINK="https://www.youtube.com/watch?v=H-sMmxfNxBM" MODIFIED="1632304727915" TEXT=""/>
+<node CREATED="1632304776176" ID="ID_1846466891" LINK="https://github.com/dieterjava/debugging-angular" MODIFIED="1632304842920" TEXT=""/>
+<node CREATED="1632401891358" ID="ID_1024238460" MODIFIED="1632401900126" TEXT="Angular debugging in VSCode">
+<node CREATED="1632401900565" ID="ID_125163332" LINK="https://newbedev.com/debug-with-visual-studio-code-not-working" MODIFIED="1632401906257" TEXT=""/>
+</node>
+</node>
+<node CREATED="1632303490191" ID="ID_179395096" MODIFIED="1632809165036" POSITION="right" TEXT="Framework from Google"/>
+<node CREATED="1632303530431" ID="ID_1077173248" MODIFIED="1632303576864" POSITION="right" TEXT="@Component({  selector: &apos;app-root&apos;,   templateUrl: &apos;./app.component.html&apos;,   styleUrls: [&apos;./app.component.css&apos;] })"/>
+<node CREATED="1632406218740" ID="ID_844666366" MODIFIED="1632406238903" POSITION="left" TEXT="DevExtreme Samples">
+<node CREATED="1632811759940" ID="ID_1711641334" MODIFIED="1632811768643" TEXT="Documentation">
+<node CREATED="1632811079388" ID="ID_602904265" MODIFIED="1632811084213" TEXT="Angular Concepts">
+<node CREATED="1632811084684" ID="ID_1857042387" LINK="https://angular.io/guide/architecture" MODIFIED="1632811088682" TEXT=""/>
+</node>
+</node>
+<node CREATED="1632406239573" ID="ID_1570636081" MODIFIED="1632406241853" TEXT="Grid">
+<node CREATED="1632406246251" ID="ID_88591342" LINK="https://js.devexpress.com/Demos/WidgetsGallery/Demo/DataGrid/SimpleArray/Angular/Light/" MODIFIED="1632406331921" TEXT=""/>
+</node>
+</node>
+<node CREATED="1632809487645" ID="ID_808626267" MODIFIED="1632809503829" POSITION="right" TEXT="Generate: Class, Enum, Interface, Route"/>
+<node CREATED="1632809517649" ID="ID_578513011" MODIFIED="1632809551019" POSITION="right" TEXT="Generate: Component, Directive, Module, Pipe, Service"/>
+</node>
+</map>
